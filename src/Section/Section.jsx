24bfb1235fc4a6609c9e58d7fd2fff703fcb13bb
Jan Škvařil <kondr4@seznam.cz>
2020-08-02 17:30:29 +0200

    Section comments

--- src/Section/Section.jsx
+++ src/Section/Section.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Section.scss';
 const Section = (props) => {
+    //styles passed by props
     let styles = {
         boxShadow: !!props.shadow ? "0px 0px 77px -16px rgba(0,0,0,0.75)" : "none",
         background: !!props.bg ? props.bg : "#ffffff",
@@ -11,8 +12,11 @@ const Section = (props) => {
         paddingBottom: !!props.paddingBot ? props.paddingBot : "",
         paddingTop: !!props.paddingTop ? props.paddingTop : "",
     };
+
+    //classes
     let classes = "section";
     if (!!props.styled) classes += " styled";
+    //align of h tags, default is middle
     if (!!props.headline_align) {
         if (props.headline_align == "right") {
             classes += " h_right";
@@ -22,6 +26,7 @@ const Section = (props) => {
         }
     }
 
+    //left line, is not displayed by default
     let line_styles = {
         display: "none",
     }
@@ -32,12 +37,17 @@ const Section = (props) => {
     }
     return (
         <div className={classes} style={styles}>
+            {/* Content ofsection */}
             {props.children}
+            {/* Left line */}
             <div className="line" style={line_styles}></div>
         </div>
     );
 }
 
+//In section must be one of "parts" - Full/Half/row
+
+//Will be 100% width
 const Full = (props) => {
     let styles = {
         background: (!!props.bg ? props.bg : "transparent") + " center",
@@ -51,7 +61,7 @@ const Full = (props) => {
         </div>);
 }
 
-
+//will cover 50% of width
 const Half = (props) => {
     let styles = {
         background: (!!props.bg ? props.bg : "transparent") + " center",
@@ -75,6 +85,8 @@ const Half = (props) => {
         </div>);
 }
 
+//will cover 100% of with, styled for elements displayed next to each other
+//e.g. for Card component
 const Row = (props) => {
     let styles = {
         background: (!!props.bg ? props.bg : "transparent") + " center",
