6ffcc22c420002a03c5d56be114de049dbd68b35
Jan Škvařil <kondr4@seznam.cz>
2020-08-02 17:40:06 +0200

    snackmessages

--- src/SnackMessages/SnackMessages.jsx
+++ src/SnackMessages/SnackMessages.jsx
@@ -22,13 +22,13 @@ class SnackMes extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            open: false,
-            text: "",
-            type: "normal"
+            open: false, //if snackmessage is displayed
+            text: "", //content of the message
+            type: "normal" //type of message, can be "succes", "error", "normal"
         };
     }
 
-
+    //closing message
     handleClose(event, reason) {
         if (reason === 'clickaway') {
             return;
@@ -36,22 +36,29 @@ class SnackMes extends React.Component {
 
         this.setState({ open: false });
     };
+
+    //function, that opens the message called by parrent
     Open(message, type) {
         console.log(this);
         this.setState({ open: true, text: message, type: type });
     }
+
+    //so user can create reference in parrent object like this:¨
+    //<SnackMessages onRef={ref => (this.showMessage = ref)} />
+    //and then just call this.showMessage() to call Open()
     componentDidMount() {
         this.props.onRef(this.Open.bind(this))
     }
     componentWillUnmount() {
         this.props.onRef(undefined)
     }
+
     render() {
-        // openFunc = this.Open.bind(this);
         const { classes } = this.props;
         return (
             <div>
                 <Snackbar
+                    // position
                     anchorOrigin={{
                         vertical: 'bottom',
                         horizontal: 'left',
@@ -63,6 +70,7 @@ class SnackMes extends React.Component {
                         'aria-describedby': 'message-id',
                     }}
 
+                    //content of the message
                     children={
                         <SnackbarContent
                             message={this.state.text}
