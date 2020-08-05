6f22f68001716262c8e1426c37107457b752641f
Jan Škvařil <kondr4@seznam.cz>
2020-08-05 16:48:18 +0200

    Section, complete proptypes

--- src/Section/Section.jsx
+++ src/Section/Section.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './Section.scss';
 import PropTypes from 'prop-types';
 
-
+/**
+ * @brief Section component contains part components, its main building block of gabro framework website
+ * Section component has 100% width a height dependent on its content. Its generally used for text block
+ * or to contain other components (like contact form, cords, etc.).
+ */
 const Section = (props) => {
     //styles passed by props
     let styles = {
@@ -48,10 +52,30 @@ const Section = (props) => {
     );
 }
 
-
+//proptypes for section
 Section.propTypes = {
-    /** Text color teeeest */
+    // == Styles ==
+    /** If shadow should be displayed around section */
+    shadow: PropTypes.bool,
+    /** Background attribute of the section, can be color, image, gradient,... */
+    bg: PropTypes.string,
+    /** Text color */
     color: PropTypes.string,
+    /** Background-size attribute, default is "cover" */
+    bgSize: PropTypes.string,
+    /** Backdrop-filter attribute, for example blur(5px) will blur the background */
+    bgFilter: PropTypes.string,
+    /** Padding-bottom attribute, for example 100px */
+    paddingBot: PropTypes.string,
+    /** Padding-top attribute, for example 100px */
+    paddingTop: PropTypes.string,
+    // == Other ==
+    /** If framework styles should be applied on content, default is "false" */
+    styled: PropTypes.bool,
+    /** Where should be headlines aligned. If not given, default will be middle */
+    headline_align: PropTypes.oneOf("left", "right"),
+    /** Background attribute (color, gradient) of Left line. Line will not be displayed if empty */
+    line: PropTypes.string,
 }
 
 //In section must be one of "parts" - Full/Half/row
