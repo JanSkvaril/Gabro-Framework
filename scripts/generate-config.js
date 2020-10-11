fbbc78f9d5b9e90210fa53bff342236d06d85f2e
Jan Škvařil <kondr4@seznam.cz>
2020-10-11 18:07:26 +0200

    Add basic html tags

--- scripts/generate-config.js
+++ scripts/generate-config.js
@@ -56,12 +56,73 @@ for (let component of components) {
         can_be_in: component.can_be_in
     }
 }
+let can_be_in_text = ["Full", "Half", "Footer", "LandingPage", "Card", "ContactForm"];
+let text_can_be_in = [...can_be_in_text];
+
+
+text_can_be_in.push("h1");
+result.components["h1"] = {
+    props: [{
+        "name": "children?",
+        "type": "string"
+    }],
+    can_be_in: ["LandingPage"]
+}
+text_can_be_in.push("h2");
+result.components["h2"] = {
+    props: [{
+        "name": "children?",
+        "type": "string"
+    }],
+    can_be_in: ["Full", "Half", "LandingPage", "Card", "ContactForm"]
+}
+text_can_be_in.push("h3");
+result.components["h3"] = {
+    props: [{
+        "name": "children?",
+        "type": "string"
+    }],
+    can_be_in: ["Full", "Half"]
+}
+text_can_be_in.push("h4");
+result.components["h4"] = {
+    props: [{
+        "name": "children?",
+        "type": "string"
+    }],
+    can_be_in: ["Full", "Half", ]
+}
+
+
+text_can_be_in.push("p");
+result.components["p"] = {
+    props: [{
+        "name": "children?",
+        "type": "string"
+    }],
+    can_be_in: can_be_in_text
+}
+
+text_can_be_in.push("a");
+result.components["a"] = {
+    props: [{
+            "name": "href",
+            "type": "string"
+        },
+        {
+            "name": "children?",
+            "type": "string"
+        }
+    ],
+    can_be_in: can_be_in_text
+}
+
 result.components["Text"] = {
     props: [{
         "name": "children?",
         "type": "ritch"
     }],
-    can_be_in: ["Full", "Half", "Footer", "LandingPage", "Card", "ContactForm"]
+    can_be_in: text_can_be_in
 }
 
 fs.writeFile("./GabroConfig.json", JSON.stringify(result), (e) => {
